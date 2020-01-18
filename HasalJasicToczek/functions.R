83d6ce1cedc4a95f90d67e1d02f3ba56bde6e562
KacperToczek <56823907+KacperToczek@users.noreply.github.com>
2020-01-18 19:08:27 +0100

    Update functions.R

--- HasalJasicToczek/functions.R
+++ HasalJasicToczek/functions.R
@@ -1,8 +1,8 @@
 
 library(ggplot2)
 # Wczytanie danych
-data <- read.table("german.data")
-colnames(data) <- c("Account_status",
+credit_data <- read.table("german.data")
+colnames(credit_data) <- c("Account_status",
                     "Duration(month)",
                     "Credit_history",
                     "Puprose",
@@ -25,66 +25,43 @@ colnames(data) <- c("Account_status",
                     "Decision")
 
 # Zmiana wieku na przedziały wieku.
-new_age <- c(1:1000)
-i=1
-for(k in data$Age){
-  if (k <= 20) new_age[i] = "<20"
-  if (k>20 && k <= 25) new_age[i] = "20-25"
-  if (k>25 && k <= 30) new_age[i] = "25-30"
-  if (k>30 && k <= 35) new_age[i] = "30-35"
-  if (k>35 && k <= 40) new_age[i] = "35-40"
-  if (k>40 && k <= 50) new_age[i] = "40-50"
-  if (k>50 && k <= 60) new_age[i] = "50-60"
-  if (k>60) new_age[i] = ">60"
-  i=i+1
+partition_age <- function(range_age){
+  cut(credit_data$Age, breaks = seq(18,80,range_age))
 }
-data$Age <- new_age
+credit_data$Age <- partition_age(5)
 
 # Zmiana decyzji 
-positive <- which(data$Decision == 1)
-negative <- which(data$Decision == 2)
-data$Decision[positive] = "Granted"
-data$Decision[negative] = "Not granted"
+positive <- which(credit_data$Decision == 1)
+negative <- which(credit_data$Decision == 2)
+credit_data$Decision[positive] = "Granted"
+credit_data$Decision[negative] = "Not granted"
 
 # Zmiana kwoty na przedziały kwoty
-new_amount <- c(1:1000)
-i=1
-for(k in data$Credit_amount){
-  if (k <= 1000) new_amount[i] = " <1000"
-  if (k>1000 && k <= 1500) new_amount[i] = " 1000-1500"
-  if (k>1500 && k <= 2000) new_amount[i] = " 1500-2000"
-  if (k>2000 && k <= 2500) new_amount[i] = " 2000-2500"
-  if (k>2500 && k <= 3000) new_amount[i] = " 2500-3000"
-  if (k>3000 && k <= 3500) new_amount[i] = " 3000-3500"
-  if (k>3500 && k <= 4000) new_amount[i] = " 3500-4000"
-  if (k>4000 && k <= 5000) new_amount[i] = " 4000-5000"
-  if (k>5000 && k <= 7500) new_amount[i] = " 5000-7500"
-  if (k>7500 && k <= 10000) new_amount[i] = " 7500-10000"
-  if (k>10000) new_amount[i] = ">10000"
-  i=i+1
+partition_credit_amount <-function(range_amount){
+  cut(credit_data$Credit_amount, breaks = seq(0,20000,range_amount))
 }
-data$Credit_amount <- new_amount
+credit_data$Credit_amount <- partition_credit_amount(2000) #przedziały w dziwnej postaci
 
 changing_factor_names <- function(col, new_names){
-  levels(data$col) <- new_names
+  levels(credit_data$col) <- new_names
 }
 
 #sapply(colnames(data), changing_factor_names, c(c('<0DM', '[0,200]DM'))
 
 plot_one_attribute <- function(col){
-  ggplot(data, aes_string( x = col, fill = 'Decision')) + 
+  ggplot(credit_data, aes_string( x = col, fill = 'Decision')) + 
     geom_bar(position = "dodge") + 
     labs(title = paste("Decision about credit depending on", col))
 }
 
 plot_two_attributes <- function(col1, col2){
-  ggplot(data, aes_string( x = col1, fill = col2, color = 'Decision')) + 
+  ggplot(credit_data, aes_string( x = col1, fill = col2, color = 'Decision')) + 
     geom_bar(position = "dodge") + 
     #facet_wrap(~)
     labs(title = paste("Decision about credit depending on", col1, 'and', col2))
   #grubość ramek i inne kolory ramek niż kolumn
 }
 
-plot_one_attribute('Account_status')
-plot_two_attributes('Job', 'Account_status')
+plot_one_attribute('Credit_amount')
+#plot_two_attributes('Job', 'Account_status')
 
