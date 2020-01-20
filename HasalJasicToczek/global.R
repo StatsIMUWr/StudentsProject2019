52f9722961a7d5ccb95fc2be0448af06a4e353a7
KacperToczek <56823907+KacperToczek@users.noreply.github.com>
2020-01-20 14:06:14 +0100

    Update global.R

--- HasalJasicToczek/global.R
+++ HasalJasicToczek/global.R
@@ -23,47 +23,45 @@ colnames(credit_data) <- c("Account_status",
                            "Foreign_worker",
                            "Decision")
 
-# Zmiana wieku na przedziały wieku.
-#partition_age <- function(range_age){
-#cut(credit_data$Age, breaks = seq(18,80,range_age))
-#}
-#credit_data$Age <- partition_age(10)
-
-age_range <- function(st_age, nd_age, by){
-  credit_data$Age <- cut(credit_data$Age, breaks = seq(st_age, nd_age, by))
-}
-
 # Zmiana decyzji 
 positive <- which(credit_data$Decision == 1)
 negative <- which(credit_data$Decision == 2)
 credit_data$Decision[positive] = "Granted"
 credit_data$Decision[negative] = "Not granted"
 
-# Zmiana kwoty na przedziały kwoty
-partition_credit_amount <-function(range_amount){
-  credit_data$Credit_amount <- cut(credit_data$Credit_amount, breaks = seq(0,20000,range_amount))
+# Zmiana wieku na przedziały wieku.
+age_range <- function(st_age, nd_age, by) {
+  credit_data_age <- credit_data[credit_data$Age >= st_age & credit_data$Age <= nd_age,]
+  credit_data_age$Age <- cut(credit_data_age$Age, breaks = seq(st_age, nd_age, by))
+  credit_data_age
 }
 
-changing_factor_names <- function(col, new_names){
-  levels(credit_data$col) <- new_names
+
+# Zmiana kwoty na przedziały kwoty
+partition_credit_amount <-function(range_amount){
+  credit_data_age$Credit_amount <- cut(credit_data_age$Credit_amount, breaks = seq(0,20000,range_amount))
 }
+
+# changing_factor_names <- function(credit_data, col, new_names){
+#   levels(credit_data[[col]]) <- new_names
+#   credit_data
+# }
 
 #sapply(colnames(data), changing_factor_names, c(c('<0DM', '[0,200]DM'))
 
-plot_one_attribute <- function(col){
-  ggplot(credit_data, aes_string( x = col, fill = 'Decision')) + 
+plot_one_attribute <- function(credit_data_age,col){
+  ggplot(credit_data_age, aes_string( x = col, fill = 'Decision')) + 
     geom_bar(position = "dodge") + 
     labs(title = paste("Decision about credit depending on", col))
 }
 
-plot_two_attributes <- function(col1, col2){
-  ggplot(credit_data, aes_string( x = col1, fill = col2, color = 'Decision')) + 
+plot_two_attributes <- function(credit_data_age, col1, col2){
+  ggplot(credit_data_age, aes_string( x = col1, fill = col2, color = 'Decision')) + 
     geom_bar(position = "dodge") + 
     #facet_wrap(~)
     labs(title = paste("Decision about credit depending on", col1, 'and', col2))
   #grubość ramek i inne kolory ramek niż kolumn
 }
-
-plot_one_attribute('Credit_amount')
-plot_two_attributes('Job', 'Account_status')
-
+# 
+# plot_one_attribute('Credit_amount')
+# plot_two_attributes('Job', 'Account_status')
