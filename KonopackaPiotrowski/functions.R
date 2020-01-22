3f020b9f020426becb47bfa77bb0189df5fbfd96
brzoskwinka <konopackamartyna@gmail.com>
2020-01-22 22:23:55 +0100

    adds age and year intervals

--- KonopackaPiotrowski/functions.R
+++ KonopackaPiotrowski/functions.R
@@ -7,7 +7,7 @@ load_libraries <- function(libs){
 }
 
 #load libraries that may be useful
-libs <- list("conflicted", "dplyr", "lubridate", "tidyr", "stringr", "data.table", "ggplot2")
+libs <- list("plotly", "conflicted", "dplyr", "lubridate", "tidyr", "stringr", "data.table", "ggplot2")
 load_libraries(libs)
 
 #needs data.table library. Loads data and removes duplicated rows.
@@ -28,10 +28,10 @@ load_as_dataframe <- function(path){
 }
 
 #load data without duplicated rows 
-data <- load_as_dataframe("data/athlete_events.csv")
+data_NA <- load_as_dataframe("data/athlete_events.csv")
 
 #find all NAs
-is_NA_dtf <- as_tibble(is.na.data.frame(data))
+is_NA_dtf <- as_tibble(is.na.data.frame(data_NA))
 is_NA_sum <- colSums(is_NA_dtf)
 
 #boolean vectors showing where variables aren't NAs
@@ -40,9 +40,21 @@ clear_weight <- !is_NA_dtf$Weight
 clear_height <- !is_NA_dtf$Height
 medals_only <- !is_NA_dtf$Medal
 
+#create dataframe without NA age, height and weight (NA medals allowed)
+data_clear <- data_NA[clear_weight & clear_height & clear_age, ]
+
+#add decade column
+Decade <- factor((data_clear$Year%/%100)*100)
+data_clear <- cbind(data_clear, Decade)
+
+#add age groups
+#TODO fix NA Age_Group
+intervals <- c(11,13,15,17,19,21,23,27,29,31,36,41,46,50,60,71)
+Age_Group <- factor(cut(data_clear$Age, intervals))
+data_clear <- cbind(data_clear, Age_Group)
+
 #weight vs height for athletes in age between ... ...
 plot1 <- function(low, high){
-  no_NA_body <- data[clear_weight & clear_height & clear_age, ]
   range <- low <= no_NA_body$Age & no_NA_body$Age <= high
   age_str <- paste("in age between ", as.character(low), "and", as.character(high))
   
@@ -55,7 +67,7 @@ plot1 <- function(low, high){
 #histogram: how many observations in column x colored by...
 #TODO change position, color etc.
 plot2 <- function(variable, color_by){
-  ggplot(data, aes_string(x = variable, fill = color_by)) + 
+  ggplot(data_clear, aes_string(x = variable, fill = color_by)) + 
     geom_bar() +
     scale_fill_manual("Sex", values = c(F = "sienna3", M = "navy")) +
     labs(title = "Number of observations in our database", 
@@ -69,4 +81,6 @@ plot2 <- function(variable, color_by){
 #3.czy da sie zrobic tak ze jak najade na slupek to wyswietli mi konkretna wartosc
 #4.https://shiny.rstudio.com/gallery/navbar-example.html?fbclid=IwAR0hGM1ay5ODMSqTJs4jgrAZNNf8cScKwcOiy4c1DF1n6KdhQEaxw5S2qjI
 #5.TODO naprawic plot2
+
+#TODO: 
 
