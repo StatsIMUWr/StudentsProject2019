c411fc3fe323b638f46b3c9b3ff90faeb029955d
KacperToczek <56823907+KacperToczek@users.noreply.github.com>
2020-01-28 22:20:11 +0100

    mozliwosc pobierania wykresu

--- HasalJasicToczek/server.R
+++ HasalJasicToczek/server.R
@@ -1,13 +1,22 @@
-server <- function(input, output) {
-  output$one_variable <- renderPlot({
+server <- function(input, output){
+  output$plot <- renderPlot({
     credit_data_age <- age_range(input$age_range[1],input$age_range[2], input$step)
     credit_data_age <- partition_credit_amount(credit_data_age, input$amount)
-    plot_one_attribute(credit_data_age, input$parametr, input$position, input$color, input$font)
-    
+    if(input$tabset == "one variable"){
+      plot_one_attribute(credit_data_age, input$parametr, input$position, input$color, input$font)
+    } else{
+      plot_two_attributes(credit_data_age, input$parametr, input$parametr2, input$position, input$color, input$font)
+    }
   })
-  output$two_variables <- renderPlot({
-    credit_data_age <- age_range(input$age_range[1],input$age_range[2], input$step)
-    credit_data_age <- partition_credit_amount(credit_data_age, input$amount)
-    plot_two_attributes(credit_data_age, input$parametr, input$parametr2, input$position, input$color, input$font)
-  })
+  output$downloadPlot <- downloadHandler(
+    filename = function(){
+      paste(input$parametr, "png", sep = ".")
+    },
+    content = function(filename){
+      credit_data_age <- age_range(input$age_range[1],input$age_range[2], input$step)
+      credit_data_age <- partition_credit_amount(credit_data_age, input$amount)
+      ggsave(file =filename,plot_one_attribute(credit_data_age, input$parametr, input$position, input$color, input$font))
+    }
+  )
+  
 }
