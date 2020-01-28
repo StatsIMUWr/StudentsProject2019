7151e54ca398abb93618792c1362ee1b4843234d
hasalk <56825140+hasalk@users.noreply.github.com>
2020-01-28 18:11:32 +0100

    adds fonts, improve string outputs

--- HasalJasicToczek/global.R
+++ HasalJasicToczek/global.R
@@ -2,6 +2,11 @@ library(shiny)
 library(ggplot2)
 library(ggthemes)
 library(plyr)
+library(stringr)
+library(extrafont)
+
+font_import()
+loadfonts(device = 'win')
 
 credit_data <- read.table("german.data")
 colnames(credit_data) <- c("Account_status",
@@ -26,6 +31,9 @@ colnames(credit_data) <- c("Account_status",
                            "Foreign_worker",
                            "Decision")
 
+parameters <- colnames(credit_data)
+names(parameters) <- str_replace_all(colnames(credit_data), pattern = '_', replacement = ' ')
+
 
 
 # Zmiana decyzji 
@@ -36,8 +44,8 @@ credit_data$Decision[negative] = "Not granted"
 
 
 vector_factor_col <-c("Account_status",
-                     "Credit_history",
-                     "Puprose")
+                      "Credit_history",
+                      "Puprose")
 
 # "Savings_account_and_bonds",
 # "Present_employment_since",
@@ -54,33 +62,28 @@ vector_factor_col <-c("Account_status",
 # "Foreign_worker")
 # credit_data <- change_factor_names('Account_status', c('<0DM', '[0,200]DM', '> 200DM', 'none'))
 new_names <- list(c('<0DM', '[0,200]DM', '> 200DM', 'none'),
-               c('none/all paid back duly',
-                 'all paid back duly(in this bank',
-                 'all paid back duly(until now)',
-                 'delay in past',
-                 'critical/other credits existing'),
-               c('car(new)',
-                 'car(used)',
-                 'furniture/equpment',
-                 'radio/tv',
-                 'domestic appliances',
-                 'repairs',
-                 'education',
-                 'retraining',
-                 'business',
-                 'other'))
+                  c('none/all paid back duly',
+                    'all paid back duly(in this bank',
+                    'all paid back duly(until now)',
+                    'delay in past',
+                    'critical/other credits existing'),
+                  c('car(new)',
+                    'car(used)',
+                    'furniture/equpment',
+                    'radio/tv',
+                    'domestic appliances',
+                    'repairs',
+                    'education',
+                    'retraining',
+                    'business',
+                    'other'))
 
 for(k in 1:length(new_names)) {
   credit_data[,vector_factor_col[k]] <-  mapvalues(credit_data[,vector_factor_col[k]],
                                                    from = levels(credit_data[,vector_factor_col[k]]),
                                                    to = new_names[[k]])
 }
-# change_factor_names <- function(i) {
-#   levels(credit_data[[vector_factor_col[i]]]) <- new_names[i]
-#   credit_data
-# } #forcats
 
-#sapply(c(1:3), change_factor_names)
 
 # Zmiana wieku na przedzialy wieku.
 age_range <- function(st_age, nd_age, by) {
@@ -97,28 +100,34 @@ partition_credit_amount <-function(credit_data_age, range_amount) {
 
 
 
-plot_one_attribute <- function(credit_data_age,col,fill_stack_dodge, dark2_set1_pastel2 ) {
+plot_one_attribute <- function(credit_data_age,col,fill_stack_dodge, dark2_set1_pastel2, cfont) {
   ggplot(credit_data_age, aes_string( x = col, fill = 'Decision')) + 
     geom_bar(position = fill_stack_dodge) + 
     scale_fill_brewer(palette = dark2_set1_pastel2) +
     theme_stata() +
-    labs(title = paste("Decision about credit depending on", col)) +
-    theme(text=element_text(size=15)) 
+    labs(title = paste("Decision about credit depending on", str_replace_all(col, '_', ' '))) +
+    theme(text=element_text(size=15, family = cfont))+
+    xlab(str_replace_all(col, '_', ' '))
 }
 
-plot_two_attributes <- function(credit_data_age, col1, col2, fill_stack_dodge, dark2_set1_pastel2) {
+plot_two_attributes <- function(credit_data_age, col1, col2, fill_stack_dodge, dark2_set1_pastel2, cfont) {
   ggplot(credit_data_age, aes_string( x = col1, fill = col2)) + 
     geom_bar(position = fill_stack_dodge ) +
     scale_fill_brewer(palette = dark2_set1_pastel2) +
     theme_stata() +
-    #theme(axis.title = element_text(color = 'blue')) +
     facet_wrap(vars(credit_data_age$"Decision")) +
-    labs(title = paste("Decision about credit depending on", col1, 'and', col2)) +
-    theme(text=element_text(size=15)) 
+    labs(title = paste("Decision about credit depending on", str_replace_all(col1, '_', ' '),
+                       'and', str_replace_all(col2, '_', ' '))) +
+    theme(text=element_text(size=15, family = cfont))  +
+    xlab(str_replace_all(col1, '_', ' ') )
 }
 
 vector_position <- c('fill', 'stack', 'dodge')
-vector_color <- c('Set1', 'Dark2', 'Pastel2')
+names(vector_position) <- c('Proportion', 'Count together', 'Count in row')
+vector_color <- c('Set3','Set1', 'Dark2', 'Pastel2')
+names(vector_color) <- c('Default', 'Normal', 'Dark', 'Pastel')
+vector_fonts <- c('serif', 'Bahnschrift', 'Comic Sans MS', 'Javanese Text')
 
-plot_one_attribute(credit_data, 'Credit_history', 'dodge', 'Dark2')
-plot_two_attributes(credit_data,'Job', 'Account_status', 'stack', 'Pastel2')
+plot_one_attribute(credit_data, 'Credit_history', 'dodge', 'Dark2', 'serif')
+plot_two_attributes(credit_data, 'Account_status', 'Job', 'stack', 'Pastel2', 'serif')
+
--- HasalJasicToczek/server.R
+++ HasalJasicToczek/server.R
@@ -2,12 +2,12 @@ server <- function(input, output) {
   output$one_variable <- renderPlot({
     credit_data_age <- age_range(input$age_range[1],input$age_range[2], input$step)
     credit_data_age <- partition_credit_amount(credit_data_age, input$amount)
-    plot_one_attribute(credit_data_age, input$parametr, input$fill, input$color)
+    plot_one_attribute(credit_data_age, input$parametr, input$position, input$color, input$font)
     
   })
   output$two_variables <- renderPlot({
     credit_data_age <- age_range(input$age_range[1],input$age_range[2], input$step)
     credit_data_age <- partition_credit_amount(credit_data_age, input$amount)
-    plot_two_attributes(credit_data_age, input$parametr, input$parametr2, input$fill, input$color)
+    plot_two_attributes(credit_data_age, input$parametr, input$parametr2, input$position, input$color, input$font)
   })
 }
