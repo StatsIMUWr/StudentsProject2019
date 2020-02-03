0fdda89da9d81270bf202ce3fe8188ab7566994b
wicix <Wiktorj99@gmail.com>
2020-02-03 11:05:24 +0100

    adds presentations and deletes unnecessary comments in global

--- BuszkaJacaszekJeszka/global.R
+++ BuszkaJacaszekJeszka/global.R
@@ -52,12 +52,12 @@ add_weekday <- function(streaming_history){
 #### creating and preparing dataframe
 make_streaming_history_complete <- function(folder_path){
   make_streaming_history_df(folder_path) %>%
-  names_change() %>% 
-  mutate(end_time = ymd_hm(end_time)) %>% 
-  mutate(s_played = dmilliseconds((s_played))) %>%
-  add_start_time() %>%
-  add_skipped() %>%
-  add_weekday()
+    names_change() %>% 
+    mutate(end_time = ymd_hm(end_time)) %>% 
+    mutate(s_played = dmilliseconds((s_played))) %>%
+    add_start_time() %>%
+    add_skipped() %>%
+    add_weekday()
   
 }
 
@@ -235,7 +235,6 @@ number_of_songs_listened_by_hour <- function(streaming_history, start_date, end_
   vis
 }
 
-number_of_songs_listened_by_hour(streaming_history,"2019-09-10", "2019-10-09",FALSE, FALSE )
 
 # number_of_songs_listened_by_weekday(streaming_History, "2019-06-10", "2019-10-09")
 ##visualizes number of songs listened by weekday in given time period
@@ -266,8 +265,6 @@ number_of_skipped_songs <- function(streaming_history, start_date, end_date, by 
   if (type == "bar") vis <- vis + geom_bar(fill = "sienna4")
   vis
 } 
-
-number_of_skipped_songs(streaming_history, "2019-07-10", "2019-10-18", by = "day", "bar")
 
 ### function to be used on search Queries:
 
@@ -305,7 +302,7 @@ platform_used_by_date<- function(search_queries, start_date, end_date){
       panel.background = element_rect(fill = "moccasin"),
       legend.key = element_rect(fill = "moccasin"), 
       axis.text.x = element_text(angle = 0)
-      ) +
+    ) +
     xlab("Date") +
     ylab("How many searches") +
     scale_color_brewer(palette="Accent")
@@ -321,81 +318,3 @@ country_by_date <- function(search_queries, start_date, end_date){
     ylab("How many searches") +
     scale_color_brewer(palette="Accent")
 }
-##visualizes number of songs played in given time period at different hours
-
-
-number_of_songs_listened_by_hour <- function(streaming_history, start_date, end_date,
-                                             by_weekday = FALSE, dont_show_skipped = TRUE){
-  filtered <- filter(streaming_history, start_time >= ymd(start_date), start_time <= ymd(end_date))
-  if (dont_show_skipped) filtered <- filter(filtered, skipped == FALSE)
-  vis <- ggplot(filtered, aes(x = hour(start_time))) +
-    geom_bar() +
-    scale_x_discrete(limits = 0:24) +
-    xlab("Hour") +
-    ylab("Songs listened")
-
-  if (by_weekday){
-    vis <- vis+
-      facet_wrap(~weekday)
-  }
-  vis
-}
-
-##visualizes number of songs listened by weekday in given time period
-number_of_songs_listened_by_weekday <- function(streaming_history, start_date, end_date, dont_show_skipped = TRUE){
-  filtered <- filter(streaming_history, start_time >= ymd(start_date), start_time <= ymd(end_date))
-  if (dont_show_skipped) filtered <- filter(filtered, skipped == FALSE)
-  vis <- ggplot(filtered, aes(x = weekday)) +
-    geom_bar() +
-    xlab("Weekday") +
-    theme(axis.text.x = element_text(angle = 270)) +
-    ylab("Songs listened")
-  vis
-}
-
-##visualizes number of songs skipped in given time period, has two vesiorns - bar and point
-
-number_of_skipped_songs <- function(streaming_history, start_date, end_date, by = "day", type = "bar"){
-  filtered <- filter(streaming_history, start_time >= ymd(start_date), start_time <= ymd(end_date), skipped == TRUE) %>%
-    mutate(end_time = floor_date(end_time, by))
-  vis <- ggplot(filtered, aes(x = end_time )) +
-    ylab("Songs skipped") +
-    xlab("Date")
-  if (type == "point") vis <- vis + geom_point(stat = "count")
-  if (type == "bar") vis <- vis + geom_bar()
-  vis
-}
-
-### function to be used on search queries:
-
-##
-platform_used <- function(search_queries, start_date, end_date){
-  filter(search_queries, date >= ymd(start_date), date <= ymd(end_date)) %>%
-    ggplot(aes(x = platform, fill = platform)) +
-    geom_bar(show.legend = FALSE) +
-    xlab("Platform used") +
-    ylab("How many searches")
-
-
-}
-
-##
-platform_used_by_date<- function(search_queries, start_date, end_date){
-  filter(search_queries, date >= ymd(start_date), date <= ymd(end_date)) %>%
-    ggplot(aes(x = date, color = platform)) +
-    geom_point(stat = "count") +
-    xlab("Date") +
-    ylab("How many searches")
-}
-
-##
-country_by_date <- function(search_queries, start_date, end_date){
-  filter(search_queries, date >= ymd(start_date), date <= ymd(end_date)) %>%
-    ggplot(aes(x = date, color = country)) +
-    geom_point(stat = "count" ) +
-    xlab("Date") +
-    ylab("How many searches")
-}
-
-
-
